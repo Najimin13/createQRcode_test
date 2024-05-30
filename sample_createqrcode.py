69ef4e38eaa5cad879beb85acd1abe56662e1a5a
Najimin <68893918+Najimin13@users.noreply.github.com>
2024-05-31 07:05:27 +0900

    Update sample_createqrcode.py

--- sample_createqrcode.py
+++ sample_createqrcode.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import io
 import qrcode
 from PIL import Image
 from flask import Flask
@@ -8,16 +9,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print("start")
-   new_dir_path = 'qrImage/'
-   if not os.path.isdir(new_dir_path):
-    os.mkdir(new_dir_path)
-   img = qrcode.make("test_createQRcode")
-   img.save('qrImage/sample_QRcode.png')
-   file_name = os.path.basename('https://createqrcode-test.onrender.com/qrImage/sample_QRcode.png')
-   response = requests.get('https://createqrcode-test.onrender.com/qrImage/sample_QRcode.png')
-   with open(file_name, 'wb') as file:
-    file.write('response.content')
+   # new_dir_path = 'qrImage/'
+   # if not os.path.isdir(new_dir_path):
+   #  os.mkdir(new_dir_path)
    
-   return ""
+   img = qrcode.make("test_createQRcode")
+   img_byte_array = io.BytesIO()
+   img.save(img_byte_array, format='PNG')
+   img_byte_array.seek(0)
+   
+   # file_name = os.path.basename('https://createqrcode-test.onrender.com/qrImage/sample_QRcode.png')
+   # response = requests.get('https://createqrcode-test.onrender.com/qrImage/sample_QRcode.png')
+   # with open(file_name, 'wb') as file:
+   #  file.write('response.content')
+   
+   return send_file(img_byte_array, mimetype='image/png', as_attachment=True, attachment_filename='generated_image.png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
   
 print("end")
