8c1eee63cb3174e661f9614c9c513d31c1f08477
Najimin <68893918+Najimin13@users.noreply.github.com>
2024-05-31 05:17:05 +0900

    Update sample_createqrcode.py

--- sample_createqrcode.py
+++ sample_createqrcode.py
@@ -3,11 +3,12 @@ from PIL import Image
 from flask import Flask
 app = Flask(__name__)
 
-img = qrcode.make("test_createQRcode")
-img.save('sample_QRcode.png')
+
 
 @app.route('/')
 def index():
+   img = qrcode.make("test_createQRcode")
+   img.save('sample_QRcode.png')
    return img.show()
   
 print("end")
