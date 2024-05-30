c3c80841f453b607032252d8404a88142719b4ae
Najimin <68893918+Najimin13@users.noreply.github.com>
2024-05-31 05:30:34 +0900

    Update sample_createqrcode.py

--- sample_createqrcode.py
+++ sample_createqrcode.py
@@ -1,3 +1,4 @@
+import os
 import qrcode
 from PIL import Image
 from flask import Flask
@@ -8,6 +9,10 @@ def index():
    print("start")
    img = qrcode.make("test_createQRcode")
    img.save('sample_QRcode.png')
+   if os.path.isfile('sample_QRcode.png'):
+    print("有り")
+else:
+    print("無し")
    return ""
   
 print("end")
