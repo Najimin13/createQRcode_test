33ae760ffefa4041ff7232b8f97200ce39e19c03
Najimin <68893918+Najimin13@users.noreply.github.com>
2024-05-31 04:53:55 +0900

    Update sample_createqrcode.py

--- sample_createqrcode.py
+++ sample_createqrcode.py
@@ -1,6 +1,13 @@
 import qrcode
 from PIL import Image
+from flask import Flask
+app = Flask(__name__)
 
 img = qrcode.make("test_createQRcode")
 img.save('sample_QRcode.png')
+
+@app.route('/')
+def index():
+   return "中川美憂"
+  
 print("end")
