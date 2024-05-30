819b764e57caf1142fad9b4c444d91c370ca4da8
Najimin <68893918+Najimin13@users.noreply.github.com>
2024-05-31 07:12:18 +0900

    Update sample_createqrcode.py

--- sample_createqrcode.py
+++ sample_createqrcode.py
@@ -9,21 +9,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print("start")
-   # new_dir_path = 'qrImage/'
-   # if not os.path.isdir(new_dir_path):
-   #  os.mkdir(new_dir_path)
    
    img = qrcode.make("test_createQRcode")
    img_byte_array = io.BytesIO()
    img.save(img_byte_array, format='PNG')
    img_byte_array.seek(0)
    
-   # file_name = os.path.basename('https://createqrcode-test.onrender.com/qrImage/sample_QRcode.png')
-   # response = requests.get('https://createqrcode-test.onrender.com/qrImage/sample_QRcode.png')
-   # with open(file_name, 'wb') as file:
-   #  file.write('response.content')
-   
-   return send_file(img_byte_array, mimetype='image/png', as_attachment=True, download_name='generated_image.png')
+   return send_file(img_byte_array, mimetype='image/png', as_attachment=True, download_name='test_createQRcode.png')
 
 if __name__ == '__main__':
     app.run(debug=True)
