73a1b03b26e7ef75820b20a3138313ce835684ed
Najimin <meganemgn2@gmail.com>
2024-06-06 06:10:48 +0900

    wave2

--- sample_createqrcode.py
+++ sample_createqrcode.py
@@ -3,20 +3,29 @@ import requests
 import io
 import qrcode
 from PIL import Image
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print("start")
-   
-   img = qrcode.make("test_createQRcode")
-   img_byte_array = io.BytesIO()
-   img.save(img_byte_array, format='PNG')
-   img_byte_array.seek(0)
-   
-   return send_file(img_byte_array, mimetype='image/png', as_attachment=True, download_name='test_createQRcode.png')
+   return render_template('index.html', title='おためし')
+  
 
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+   print("start")
+
+   if request.method == 'POST':
+        name = request.form['miyuu']
+        img = qrcode.make(name)
+        img_byte_array = io.BytesIO()
+        img.save(img_byte_array, format='PNG')
+        img_byte_array.seek(0)   
+        return send_file(img_byte_array, mimetype='image/png', as_attachment=True, download_name='test_createQRcode.png')
+        
+   if request.method == 'GET':
+      return render_template('resultGet.html', title='おためしGET')
+        
 if __name__ == '__main__':
     app.run(debug=True)
   
