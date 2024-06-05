f1cdc007d3177946f36fbd2146efd363d0d5313d
Najimin <meganemgn2@gmail.com>
2024-06-06 08:02:06 +0900

    Penata APIおためし

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const PINATA_API_KEY = process.env.PINATA_API_KEY;
+const PINATA_API_SECRET = process.env.PINATA_API_SECRET;
+
+const pinataSDK = require('@pinata/sdk');
+const pinata = new pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
+
+const fs = require('fs');
+const readableStreamForFile = fs.createReadStream('./test_createQRcode.png');
+const options = {
+    pinataMetadata: {
+        //nameは必須
+        name: 'Cats',
+        //keyvaluesは必須ではない
+        keyvalues: {　　　　
+            customKey: 'customValue',
+            customKey2: 'customValue2'
+        }
+    },
+    pinataOptions: {
+        cidVersion: 0
+    }
+};
+pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
+    //handle results here
+    console.log(result);
+}).catch((err) => {
+    //handle error here
+    console.log(err);
+});
