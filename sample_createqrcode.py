33400988478c5428c431655b979b207470139d9b
Najimin <meganemgn2@gmail.com>
2024-06-14 03:45:14 +0900

    node.jsを使わないようにした

--- sample_createqrcode.py
+++ sample_createqrcode.py
@@ -15,8 +15,6 @@ def index():
 @app.route('/result', methods=['GET', 'POST'])
 def result():
    print("start")
-   RIPPLE_API_COMMAND = "index.js"
-
 
    if request.method == 'POST':
         name = request.form['miyuu']
@@ -26,9 +24,19 @@ def result():
         img_byte_array.seek(0)   
         img.save('test_createQRcode.png')
         
-
-        subprocess.check_call('node index.js', shell=True)
-        
+        with open('test_createQRcode.png', "rb") as q:
+	        url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+	        files = [
+	        ('file', ('test_createQRcode.png',q )),
+	        ]
+	        headers = {
+	        'pinata_api_key': "7889b920748ef631b2b0",
+	        'pinata_secret_api_key': "d8f3b545743f4760ec2937d3faf4df60b481a28b9b7d1c83c16ba72685455db4"
+	        }
+	        response = requests.request("POST", url, files=files, headers=headers)
+	        print(response.text)
+	        
+	        q.close()
         os.remove('test_createQRcode.png')
             
         return render_template('index.html', title='おためし成功？')
@@ -38,5 +46,3 @@ def result():
         
 if __name__ == '__main__':
     app.run(debug=True)
-  
-print("end")
